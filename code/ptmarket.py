07057e147ff570093c94ff952d389f2845bdc5ee
katerina-merkulova <ekaterina1.merkulova@intel.com>
2021-07-27 16:25:48 +0300

    first configuration

---
+++ code/ptmarket.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2020-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+"""You may copy this file as the starting point of your own model."""
+from torch.utils.data import DataLoader
+
+import data.transforms as T
+from data.dataset_loader import ImageDataset
+from data.datasets import Market1501
+from data.samplers import RandomIdentitySampler
+
+from openfl.federated import PyTorchDataLoader
+
+
+class PyTorchMarket(PyTorchDataLoader):
+    """PyTorch data loader for MNIST dataset."""
+
+    def __init__(self, data_path, batch_size, **kwargs):
+
+        """
+        Instantiate the data object.
+        Args:
+            batch_size: Size of batches used for all data loaders
+            kwargs: consumes all un-used kwargs
+        Returns:
+            None
+        """
+        super().__init__(batch_size, **kwargs)
+
+        self.dataset = Market1501(root=data_path, split_id=0,
+                                  cuhk03_labeled=False,
+                                  cuhk03_classic_split=False)
+
+        self.transform_train = T.Compose([
+            T.RandomCroping(256, 128, p=0.5),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            T.RandomErasing(probability=0.5)
+        ])
+        self.transform_test = T.Compose([
+            T.Resize((265, 128)),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+        self.batch_size = batch_size
+        self.X_train = None
+        self.y_train = None
+        self.X_valid = None
+        self.y_valid = None
+
+    def get_feature_shape(self):
+        """Get the shape of an example feature array.
+        Returns:
+            tuple: shape of an example feature array
+        """
+        raise NotImplementedError
+
+    def get_train_loader(self):
+        """
+        Get training data loader.
+        Returns
+        -------
+        loader object
+        """
+        return DataLoader(ImageDataset(self.dataset.train, transform=self.transform_train),
+                          sampler=RandomIdentitySampler(self.dataset.train, num_instances=4),
+                          batch_size=64, num_workers=4,
+                          pin_memory=True, drop_last=True)
+
+    def get_query_loader(self):
+        """
+        Get query data loader.
+        Returns:
+            loader object
+        """
+        return DataLoader(ImageDataset(self.dataset.train, transform=self.transform_test),
+                          batch_size=512, num_workers=4,
+                          pin_memory=True, drop_last=False, shuffle=False)
+
+    def get_gallery_loader(self):
+        """
+        Get gallery data loader.
+        Returns:
+            loader object
+        """
+        return DataLoader(ImageDataset(self.dataset.gallery, transform=self.transform_test),
+                          batch_size=512, num_workers=4,
+                          pin_memory=True, drop_last=False, shuffle=False)
+
+    def get_train_data_size(self):
+        """
+        Get total number of training samples.
+        Returns:
+            int: number of training samples
+        """
+        raise NotImplementedError
+
+    def get_valid_data_size(self):
+        """
+        Get total number of validation samples.
+        Returns:
+            int: number of validation samples
+        """
+        raise NotImplementedError
---
+++ code/tools.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2020-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import numpy as np
+import torch
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value.
+
+       Code imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def compute_ap_cmc(index, good_index, junk_index):
+    ap = 0
+    cmc = np.zeros(len(index))
+
+    # remove junk_index
+    mask = np.in1d(index, junk_index, invert=True)
+    index = index[mask]
+
+    # find good_index index
+    ngood = len(good_index)
+    mask = np.in1d(index, good_index)
+    rows_good = np.argwhere(mask == True)
+    rows_good = rows_good.flatten()
+
+    cmc[rows_good[0]:] = 1.0
+    for i in range(ngood):
+        d_recall = 1.0 / ngood
+        precision = (i + 1) * 1.0 / (rows_good[i] + 1)
+        # if rows_good[i]!=0:
+        #     old_precision = i*1.0/rows_good[i]
+        # else:
+        #     old_precision=1.0
+        # ap = ap + d_recall*(old_precision + precision)/2
+        ap = ap + d_recall * precision
+
+    return ap, cmc
+
+
+def evaluate(distmat, q_pids, g_pids, q_camids, g_camids):
+    num_q, num_g = distmat.shape
+    index = np.argsort(distmat, axis=1) # from small to large
+
+    num_no_gt = 0 # num of query imgs without groundtruth
+    num_r1 = 0
+    CMC = np.zeros(len(g_pids))
+    AP = 0
+
+    for i in range(num_q):
+        # groundtruth index
+        query_index = np.argwhere(g_pids==q_pids[i])
+        camera_index = np.argwhere(g_camids==q_camids[i])
+        good_index = np.setdiff1d(query_index, camera_index, assume_unique=True)
+        if good_index.size == 0:
+            num_no_gt += 1
+            continue
+        # remove gallery samples that have the same pid and camid with query
+        junk_index = np.intersect1d(query_index, camera_index)
+
+        ap_tmp, CMC_tmp = compute_ap_cmc(index[i], good_index, junk_index)
+        if CMC_tmp[0]==1:
+            num_r1 += 1
+        CMC = CMC + CMC_tmp
+        AP += ap_tmp
+
+    if num_no_gt > 0:
+        print(f'{num_no_gt} query imgs do not have groundtruth.')
+
+    CMC = CMC / (num_q - num_no_gt)
+    mAP = AP / (num_q - num_no_gt)
+
+    return CMC, mAP
+
+
+def fliplr(img):
+    ''' flip horizontal'''
+    inv_idx = torch.arange(img.size(3)-1, -1, -1).long()  # N x C x H x W
+    img_flip = img.index_select(3, inv_idx)
+
+    return img_flip
