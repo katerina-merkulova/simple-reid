2c7592ceb6c3356be5af2be164f713a86778bf77
katerina-merkulova <ekaterina1.merkulova@intel.com>
2021-08-05 20:28:12 +0300

    minor changes

--- losses.py
+++ losses.py
@@ -25,13 +25,9 @@ class ArcFaceLoss(nn.Module):
         sin_t = torch.sqrt(1.0 - cos_t * cos_t)
         cos_t_add_m = cos_t * cos_m  - sin_t * sin_m
 
-        if self.easy_margin:
-            cond = F.relu(cos_t)
-            keep = cos_t
-        else:
-            cond_v = cos_t - math.cos(math.pi - self.m)
-            cond = F.relu(cond_v)
-            keep = cos_t - math.sin(math.pi - self.m) * self.m
+        cond_v = cos_t - math.cos(math.pi - self.m)
+        cond = F.relu(cond_v)
+        keep = cos_t - math.sin(math.pi - self.m) * self.m
 
         cos_t_add_m = torch.where(cond.bool(), cos_t_add_m, keep)
 
@@ -53,10 +49,9 @@ class TripletLoss(nn.Module):
     Args:
         margin (float): margin for triplet.
     """
-    def __init__(self, margin=0.3, distance='euclidean'):
+    def __init__(self, margin=0.3, distance='cosine'):
         super(TripletLoss, self).__init__()
-        if distance not in ['euclidean', 'cosine']:
-            raise KeyError("Unsupported distance: {}".format(distance))
+
         self.distance = distance
         self.margin = margin
         self.ranking_loss = nn.MarginRankingLoss(margin=margin)
@@ -70,14 +65,8 @@ class TripletLoss(nn.Module):
         n = inputs.size(0)
 
         # Compute pairwise distance, replace by the official when merged
-        if self.distance == 'euclidean':
-            dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
-            dist = dist + dist.t()
-            dist.addmm_(1, -2, inputs, inputs.t())
-            dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
-        elif self.distance == 'cosine':
-            inputs = F.normalize(inputs, p=2, dim=1)
-            dist = - torch.mm(inputs, inputs.t())
+        inputs = F.normalize(inputs, p=2, dim=1)
+        dist = - torch.mm(inputs, inputs.t())
 
         # For each anchor, find the hardest positive and negative
         mask = targets.expand(n, n).eq(targets.expand(n, n).t())
@@ -93,3 +82,4 @@ class TripletLoss(nn.Module):
         loss = self.ranking_loss(dist_an, dist_ap, y)
 
         return loss
+
